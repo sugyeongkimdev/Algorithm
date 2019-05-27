175043ad67ae4a529bbb73dd71047af62fec3563
sugyeongkim <sugyeongkim.dev@gmail.com>
2019-05-28 08:57:03 +0900

    12910 문제 작성

---
+++ P_Algorithm/P_Algorithm/Code/learn_lessons/12910.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+// 나누어 떨어지는 숫자 배열
+// https://programmers.co.kr/learn/courses/30/lessons/12910
+
+namespace P_Algorithm_12910 {
+    public class ActiveClass {
+        public void Active() {
+            new Solution().solution(new[] { 5, 9, 7, 10 }, 5);
+            //new Solution().solution(new[] { 2, 36, 1, 3 }, 1);
+            //new Solution().solution(new[] { 3, 2, 6 }, 10);
+        }
+    }
+
+    //====================================//
+
+    public class Solution {
+        public int[] solution(int[] arr, int divisor) {
+            var search = arr.Where(a => a % divisor == 0);
+            if(search.Count() == 0) {
+                return new[]{ -1 };
+            } else {
+                var list = search.ToList();
+                list.Sort();
+                return list.ToArray();
+            }
+        }
+    }
+
+}
