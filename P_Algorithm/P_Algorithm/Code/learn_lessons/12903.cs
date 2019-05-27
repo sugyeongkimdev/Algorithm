08ea6cdc92eb9ba8356752582389ff3243817e63
sugyeongkim <sugyeongkim.dev@gmail.com>
2019-05-28 08:50:23 +0900

    12903 문제 작성

---
+++ P_Algorithm/P_Algorithm/Code/learn_lessons/12903.cs
@@ -0,0 +1,27 @@
+﻿
+// 가운데 글자 가져오기
+// https://programmers.co.kr/learn/courses/30/lessons/12901
+
+namespace P_Algorithm_12903 {
+    public class ActiveClass {
+        public void Active() {
+            new Solution().solution("abcde");
+            //new Solution().solution("qwer");
+        }
+    }
+
+    //====================================//
+
+    public class Solution {
+        public string solution(string s) {
+            if(s.Length < 2) {
+                return s;
+            } else if(s.Length % 2 == 0) {
+                return s[s.Length / 2 - 1].ToString() + s[s.Length / 2].ToString();
+            } else {
+                return s[s.Length / 2].ToString();
+            }
+        }
+    }
+
+}
