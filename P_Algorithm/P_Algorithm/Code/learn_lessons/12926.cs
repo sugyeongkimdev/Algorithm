5b7d2e33ddae7bd483ad16fd70a9074f5742f489
sugyeongkim <sugyeongkim.dev@gmail.com>
2019-05-28 19:53:55 +0900

    12926 문제 작성

---
+++ P_Algorithm/P_Algorithm/Code/learn_lessons/12926.cs
@@ -0,0 +1,34 @@
+﻿// 시저 암호
+// https://programmers.co.kr/learn/courses/30/lessons/12925
+
+namespace P_Algorithm_12926 {
+    public class ActiveClass {
+        public void Active() {
+            new Solution().solution("A", 1);
+            new Solution().solution("z", 1);
+        }
+    }
+
+    //====================================//
+
+    public class Solution {
+        public string solution(string s, int n) {
+            var arr = s.ToCharArray();
+            for(int i = 0; i < s.Length; i++) {
+                if(arr[i] == ' ') {
+                    continue;
+                }
+
+                int start = (arr[i] >= 65 && arr[i] <= 90) ? 65 : 97;
+                int end = (arr[i] >= 65 && arr[i] <= 90) ? 90 : 122;
+                if(arr[i] + n > end) {
+                    arr[i] = (char)((arr[i] + n) % end + start - 1);
+                } else {
+                    arr[i] = (char)(arr[i] + n);
+                }
+            }
+            return new string(arr);
+        }
+    }
+
+}
