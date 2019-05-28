fd651a068d8761047d7b77b6924c7e2f879899f1
sugyeongkim <sugyeongkim.dev@gmail.com>
2019-05-29 08:06:01 +0900

    12943 문제 작성

---
+++ P_Algorithm/P_Algorithm/Code/learn_lessons/Levels/Level1/12943.cs
@@ -0,0 +1,25 @@
+﻿// 콜라츠 추측
+// https://programmers.co.kr/learn/courses/30/lessons/12943
+
+namespace P_Algorithm_12943 {
+    public class ActiveClass {
+        public void Active() {
+            new Solution().solution(626331);
+        }
+    }
+
+    //====================================//
+
+    public class Solution {
+        public int solution(int num) {
+            long val = num;
+            for(int i = 0; i < 500; i++) {
+                if(val == 1) {
+                    return i;
+                }
+                val = val % 2 == 0 ? val / 2 : val * 3 + 1;
+            }
+            return -1;
+        }
+    }
+}
