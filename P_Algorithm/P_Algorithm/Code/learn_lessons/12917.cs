59cd243037491b0d59976c8fe97a8f5da161103e
sugyeongkim <sugyeongkim.dev@gmail.com>
2019-05-28 09:32:02 +0900

    12917 문제 작성

---
+++ P_Algorithm/P_Algorithm/Code/learn_lessons/12917.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+// 문자열 내림차순으로 배치하기
+// https://programmers.co.kr/learn/courses/30/lessons/12917
+
+namespace P_Algorithm_12917 {
+    public class ActiveClass {
+        public void Active() {
+            new Solution().solution("Zbcdefg");
+        }
+    }
+
+    //====================================//
+
+    public class Solution {
+        public string solution(string s) {
+            return new string(s.OrderByDescending(str => str).ToArray());
+        }
+    }
+
+}
