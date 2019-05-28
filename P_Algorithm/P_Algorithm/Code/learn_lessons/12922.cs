c655f884900e2cde397c16690148f20b06e7bf86
sugyeongkim <sugyeongkim.dev@gmail.com>
2019-05-28 17:56:45 +0900

    12922 문제 작성

---
+++ P_Algorithm/P_Algorithm/Code/learn_lessons/12922.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+// 수박수박수박수박수박수?
+// https://programmers.co.kr/learn/courses/30/lessons/12922
+
+namespace P_Algorithm_12922 {
+    public class ActiveClass {
+        public void Active() {
+            new Solution().solution(3);
+        }
+    }
+
+    //====================================//
+
+    public class Solution {
+        public string solution(int n) {
+            string str = string.Concat(Enumerable.Repeat("수박", n / 2 + 1));
+            return str.Remove(str.Length - (n % 2 == 0 ? 2 : 1));
+        }
+    }
+
+}
