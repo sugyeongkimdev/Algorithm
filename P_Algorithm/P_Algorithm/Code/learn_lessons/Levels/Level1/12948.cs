f1d40a8bf405eba8e1d562bcdb85b47fcc48cde3
sugyeongkim <sugyeongkim.dev@gmail.com>
2019-05-29 20:57:34 +0900

    12948 문제 작성

---
+++ P_Algorithm/P_Algorithm/Code/learn_lessons/Levels/Level1/12948.cs
@@ -0,0 +1,22 @@
+﻿// 핸드폰 번호 가리기
+// https://programmers.co.kr/learn/courses/30/lessons/12948
+
+namespace P_Algorithm_12948 {
+    public class ActiveClass {
+        public void Active() {
+            new Solution().solution("01033334444");
+        }
+    }
+
+    //====================================//
+
+    public class Solution {
+        public string solution(string phone_number) {
+            var arr = phone_number.ToCharArray();
+            for(int i = 0; i < arr.Length - 4; i++) {
+                arr[i] = '*';
+            }
+            return new string(arr);
+        }
+    }
+}
