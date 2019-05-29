7387e1cf5c494e35692beea2657681d4fbf50198
sugyeongkim <sugyeongkim.dev@gmail.com>
2019-05-29 21:07:18 +0900

    12950 문제 작성

---
+++ P_Algorithm/P_Algorithm/Code/learn_lessons/Levels/Level1/12950.cs
@@ -0,0 +1,24 @@
+﻿// 핸드폰 번호 가리기
+// https://programmers.co.kr/learn/courses/30/lessons/12948
+
+namespace P_Algorithm_12950 {
+    public class ActiveClass {
+        public void Active() {
+            new Solution().solution(new[,] { { 1, 2 }, { 1, 2 } }, new[,] { { 3, 4 }, { 5, 6} });
+        }
+    }
+
+    //====================================//
+
+    public class Solution {
+        public int[,] solution(int[,] arr1, int[,] arr2) {
+            var arr =new int[arr1.GetLength(0), arr1.GetLength(1)];
+            for(int i = 0; i < arr1.GetLength(0); i++) {
+                for(int j = 0; j < arr1.GetLength(1); j++) {
+                    arr[i, j] = arr1[i, j] + arr2[i, j];
+                }
+            }
+            return arr;
+        }
+    }
+}
