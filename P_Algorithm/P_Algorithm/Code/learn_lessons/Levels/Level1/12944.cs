7905174b7a0de67e95d16382c318485e9bcdeefb
sugyeongkim <sugyeongkim.dev@gmail.com>
2019-05-29 20:41:33 +0900

    12944 문제 작성

---
+++ P_Algorithm/P_Algorithm/Code/learn_lessons/Levels/Level1/12944.cs
@@ -0,0 +1,26 @@
+﻿// 평균 구하기
+// https://programmers.co.kr/learn/courses/30/lessons/12944
+
+using System.Linq;
+
+namespace P_Algorithm_12944 {
+    public class ActiveClass {
+        public void Active() {
+            new Solution().solution(new [] { 1, 2, 3, 4 });
+        }
+    }
+
+    //====================================//
+
+    public class Solution {
+        public double solution(int[] arr) {
+            //return arr.Average();
+
+            double sum = 0;
+            for(int i = 0; i < arr.Length; i++) {
+                sum += arr[i];
+            }
+            return sum/arr.Length;
+        }
+    }
+}
