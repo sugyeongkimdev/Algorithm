20713e40d35f32b5d89278514475c2e7062585e5
sugyeongkim <sugyeongkim.dev@gmail.com>
2019-05-30 10:19:45 +0900

    실수로 12969 문제를 덮어 씌운 12982 문제를 수정

--- P_Algorithm/P_Algorithm/Code/learn_lessons/Levels/Level1/12969.cs
+++ P_Algorithm/P_Algorithm/Code/learn_lessons/Levels/Level1/12969.cs
@@ -1,32 +1,30 @@
-﻿// 예산
-// https://programmers.co.kr/learn/courses/30/lessons/12982
+﻿// 직사각형 별찍기
+// https://programmers.co.kr/learn/courses/30/lessons/12969
 
 using System;
 
 namespace P_Algorithm_12969 {
     public class ActiveClass {
         public void Active() {
-            new Solution().solution(new[]{1, 3, 2, 5, 4}, 9);
+            new Solution().solution(12);
         }
     }
 
     //====================================//
 
     public class Solution {
-        public int solution(int[] d, int budget) {
-            int money = budget;
-            int count = 0;
-            int[] sort = (int[])d.Clone();
-            Array.Sort(sort);
-            for(int i = 0; i < sort.Length; i++) {
-                if(money - sort[i] < 0) {
-                    break;
-                }
-                money -= sort[i];
-                count++;
+        public void solution(int n) {
+            string[] s;
+
+            Console.Clear();
+            s = Console.ReadLine().Split(' ');
+
+            int a = Int32.Parse(s[0]);
+            int b = Int32.Parse(s[1]);
+
+            for(int i = 0; i < b; i++) {
+                Console.WriteLine("{0}", new string('*', a));
             }
-            return count;
         }
     }
-
 }
---
+++ P_Algorithm/P_Algorithm/Code/learn_lessons/Levels/Level1/12982.cs
@@ -0,0 +1,32 @@
+﻿// 예산
+// https://programmers.co.kr/learn/courses/30/lessons/12982
+
+using System;
+
+namespace P_Algorithm_12982 {
+    public class ActiveClass {
+        public void Active() {
+            new Solution().solution(new[] { 1, 3, 2, 5, 4 }, 9);
+        }
+    }
+
+    //====================================//
+
+    public class Solution {
+        public int solution(int[] d, int budget) {
+            int money = budget;
+            int count = 0;
+            int[] sort = (int[])d.Clone();
+            Array.Sort(sort);
+            for(int i = 0; i < sort.Length; i++) {
+                if(money - sort[i] < 0) {
+                    break;
+                }
+                money -= sort[i];
+                count++;
+            }
+            return count;
+        }
+    }
+
+}
