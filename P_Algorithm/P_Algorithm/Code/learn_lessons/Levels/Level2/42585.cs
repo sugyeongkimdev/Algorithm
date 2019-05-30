5912a854cb500d9d41d00c57f460bffa302bc27e
sugyeongkim <sugyeongkim.dev@gmail.com>
2019-05-31 06:12:20 +0900

    42585 문제 작성

---
+++ P_Algorithm/P_Algorithm/Code/learn_lessons/Levels/Level2/42585.cs
@@ -0,0 +1,35 @@
+﻿// 쇠막대기
+// https://programmers.co.kr/learn/courses/30/lessons/42585
+
+// 참조
+// https://aig2029.tistory.com/114
+// https://jaemin8852.tistory.com/188
+
+namespace P_Algorithm_42585 {
+    public class ActiveClass {
+        public void Active() {
+            new Solution().solution("()(((()())(())()))(())");
+        }
+    }
+
+    //====================================//
+
+    public class Solution {
+        public int solution(string arrangement) {
+            var str = arrangement.Replace("()", "0");
+            int stack = 0, count = 0;
+            for(int i = 0; i < str.Length; i++) {
+                if(str[i] == '(') {
+                    stack++;
+                } else if(str[i] == '0') {
+                    count += stack;
+                } else if(str[i] == ')') {
+                    count++;
+                    stack--;
+                }
+            }
+            return count;
+        }
+    }
+
+}
