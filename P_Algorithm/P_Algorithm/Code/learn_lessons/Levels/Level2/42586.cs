b4ba403021fa2245f45e830965b8848bae3fd164
sugyeongkim <sugyeongkim.dev@gmail.com>
2019-05-30 13:05:34 +0900

    42586 문제 작성

---
+++ P_Algorithm/P_Algorithm/Code/learn_lessons/Levels/Level2/42586.cs
@@ -0,0 +1,43 @@
+﻿// 기능개발
+// https://programmers.co.kr/learn/courses/30/lessons/42586
+
+using System.Collections.Generic;
+
+namespace P_Algorithm_42586 {
+    public class ActiveClass {
+        public void Active() {
+            new Solution().solution(new[] { 93, 30, 55 }, new[] { 1, 30, 5 });
+        }
+    }
+
+    //====================================//
+
+    public class Solution {
+        public int[] solution(int[] progresses, int[] speeds) {
+            var list = new List<int>();
+            int pro = 0;
+            while(pro < progresses.Length) {
+                int func = 0;
+
+                while(progresses[pro] < 100) {
+                    for(int i = 0; i < progresses.Length; i++) {
+                        progresses[i] += speeds[i];
+                    }
+                }
+
+                for(int i = pro; i < progresses.Length; i++) {
+                    if(progresses[i] >= 100) {
+                        pro++;
+                        func++;
+                    } else {
+                        break;
+                    }
+                }
+                list.Add(func);
+            }
+
+            return list.ToArray();
+        }
+    }
+
+}
