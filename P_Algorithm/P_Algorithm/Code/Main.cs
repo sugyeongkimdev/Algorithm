3e156a36f84b7fa728bc2b9d720aa6c370ab3937
sugyeongkim <sugyeongkim.dev@gmail.com>
2019-05-30 11:11:30 +0900

    42588 문제 작성

--- P_Algorithm/P_Algorithm/Code/Main.cs
+++ P_Algorithm/P_Algorithm/Code/Main.cs
@@ -3,7 +3,7 @@ namespace P_Algorithm {
     class Program {
         static void Main(string[] args) {
             // 알고리즘_'실행할 알고리즘 숫자'.ActiveClass().Active();
-            new P_Algorithm_49993.ActiveClass().Active();
+            new P_Algorithm_42588.ActiveClass().Active();
         }
     }
 }
