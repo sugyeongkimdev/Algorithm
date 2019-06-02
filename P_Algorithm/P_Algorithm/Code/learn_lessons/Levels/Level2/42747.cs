8f5487a67ace01cf5853ac9fda44e45167497656
sugyeongkim <sugyeongkim.dev@gmail.com>
2019-06-02 09:23:52 +0900

    42747 문제 작성

---
+++ P_Algorithm/P_Algorithm/Code/learn_lessons/Levels/Level2/42747.cs
@@ -0,0 +1,31 @@
+﻿// 큰 수 만들기
+// https://programmers.co.kr/learn/courses/30/lessons/42747
+
+// 기사를 보고 식을 만들고 다른사람 글도 읽었지만 이해가 힘듬
+// http://www.ibric.org/myboard/read.php?id=270333&Board=news
+
+using System;
+
+namespace P_Algorithm_42747 {
+    public class ActiveClass {
+        public void Active() {
+            new Solution().solution(new[]{3, 0, 6, 1, 5});
+        }
+    }
+
+    //====================================//
+    public class Solution {
+        public int solution(int[] citations) {
+            Array.Sort(citations);
+            Array.Reverse(citations);
+
+            for(int i = 0; i < citations.Length; i++) {
+                if(citations[i] <= i) {
+                    return i;
+                }
+            }
+            return citations.Length;
+        }
+    }
+
+}
