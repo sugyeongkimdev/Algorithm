98b051ca12d6a7d524ea088a258c5d6f0276e5df
sugyeongkim <sugyeongkim.dev@gamil.com>
2021-10-05 00:24:21 +0900

    Solution 81301 작성

---
+++ Script/learn_lessons/Levels/Level1/Solution81301.cs
@@ -0,0 +1,32 @@
+﻿
+// 숫자 문자열과 영단어
+// https://programmers.co.kr/learn/courses/30/lessons/81301
+
+public class Solution81301
+{
+    public Solution81301()
+    {
+        new Solution().solution("one4seveneight");
+        //new Solution().solution("23four5six7");
+        //new Solution().solution("2three45sixseven");
+        //new Solution().solution("123");
+    }
+
+    //====================================//
+    public class Solution
+    {
+        private string[] NumStrArr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+        public int solution(string s)
+        {
+            for (int i = 0; i < NumStrArr.Length; i++)
+            {
+                s = s.Replace(NumStrArr[i], i.ToString());
+            }
+            return int.Parse(s);
+        }
+    }
+    //====================================//
+}
+
+
