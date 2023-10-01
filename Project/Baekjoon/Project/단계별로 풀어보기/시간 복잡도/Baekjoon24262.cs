10626c07f9443ada2b44ac401a413386542335af
DESKTOP-BTRAG5Q\homehome <sugyeongkim.dev@gmail.com>
2023-10-01 23:24:38 +0900

    백준/시간 복잡도 추가중

---
+++ Project/Baekjoon/Project/단계별로 풀어보기/시간 복잡도/Baekjoon24262.cs
@@ -0,0 +1,21 @@
+﻿// 알고리즘 수업 - 알고리즘의 수행 시간 1
+// https://www.acmicpc.net/problem/24262
+
+using System;
+
+public class Baekjoon24262
+{
+    public Baekjoon24262 ()
+    {
+        /*
+        MenOfPassion(A[], n)
+        {
+            i = ⌊n / 2⌋;
+            return A[i]; # 코드1
+        }
+         */
+        // 위 코드는 수행횟수가 무조건 1이며 실행시간은 0이다
+        Console.WriteLine ("1");
+        Console.WriteLine ("0");
+    }
+}
